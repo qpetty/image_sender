9677581160a8cb6a23c4abc7bb10c1691a08d061
Quinton Petty <qpetty@apple.com>
2025-11-10 14:57:21 -0800

    Initial Commit

---
+++ Image_sender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Image_sender
+//
+//  Created by Quinton on 11/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Image_sender/Image_senderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_senderApp.swift
+//  Image_sender
+//
+//  Created by Quinton on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Image_senderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
